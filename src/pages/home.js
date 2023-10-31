1e7e7af1b87aefb79d1f0c5a979dcbfd1606b3f8
Dgibson89 <donovan.gibson89@gmail.com>
2023-10-31 00:31:06 -0500

    Front End work Added the ability to create recipes and will be working on getting them to display properly on the home page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRecipe();
+  }, []);
+  return (
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li> key={recipe._id}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
